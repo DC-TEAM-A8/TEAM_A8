a39291a0934e2fb9f9aa375a3cfdcb908feb2f48
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2024-05-27 14:23:23 +0900

    Created spring boot project

---
+++ src/main/java/com/its24a8/lmsrenew/LmsRenewApplication.java
@@ -0,0 +1,13 @@
+package com.its24a8.lmsrenew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LmsRenewApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LmsRenewApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/its24a8/lmsrenew/LmsRenewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.its24a8.lmsrenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LmsRenewApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
