a2053176d87d9677a1d84f2a202a73cf5712f14f
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2024-05-29 11:20:07 +0900

    日報EntityとRepositoryを作成

---
+++ src/main/java/com/its24a8/lmsrenew/data/DailyReportRepository.java
@@ -0,0 +1,6 @@
+package com.its24a8.lmsrenew.data;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DailyReportRepository extends JpaRepository<DailyReport, Long>{
+}
