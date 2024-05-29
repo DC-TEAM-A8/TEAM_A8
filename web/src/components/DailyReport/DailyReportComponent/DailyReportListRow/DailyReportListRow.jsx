fc5123076263cc706cf89d0279fed4f1f9d5285a
Masakazu.Maruta <masakazu.maruta@dreamcareer.co.jp>
2024-05-29 16:49:08 +0900

    スタイリングとコードを整えました。

--- web/src/components/DailyReport/DailyReportComponent/DailyReportListRow/DailyReportListRow.jsx
+++ web/src/components/DailyReport/DailyReportComponent/DailyReportListRow/DailyReportListRow.jsx
@@ -1,56 +1,20 @@
-/**
- */
+import React from 'react';
+import './DailyReportListRow.css';
+
 export function DailyReportListRow(props) {
   const { title, text } = props.reportData;
+  
+  const handleDelete = () => {
+    props.onDelete(title);
+  };
+
   return (
-    <div style={{
-      display: 'flex',
-      justifyContent: 'space-between', // 画面の左端と右端に子要素を配置
-      backgroundColor: 'white',
-      width: '100%',
-      padding : "10px",
-    }}>
-      {/* タイトル */}
-      <span 
-      style={{ 
-        marginRight: '10px' 
-        }}>{title}</span> 
-      {/* タイトル */}
-      {/* 本文*/}
-      <span 
-      style={{ 
-        flexGrow: 1 
-        }}>{text}</span> 
-      {/* 本文 */}
-      {/* 編集ボタンと削除ボタン*/}
-      <div 
-      style={{ 
-        display: 'flex', 
-        gap: '5px' ,
-        }}>
-        {/* 編集ボタン*/} 
-        <button onClick={props.onEdit} 
-        style={{
-          color: 'white',
-          backgroundColor: 'blue',
-          border: "none", 
-          borderRadius: "7px",
-          boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
-        }}>編集</button>
-        {/* 編集ボタン*/}
-        {/* 削除ボタン*/}
-        <button onClick={props.onDelete(props.title)} 
-        style={{
-          color: 'white',
-          backgroundColor: 'red',
-          border: "none", 
-        borderRadius: "7px",
-        boxShadow: "0 2px 4px rgba(0, 0, 0, 0.2)",
-        }}>削除</button>
-        {/* 削除ボタン*/}
+    <div className="daily-report-list-row">
+      <span className="title">{title} {text}</span>
+      <div className="button-container">
+        <button className="edit-button" onClick={props.onEdit}>編集</button>
+        <button className="delete-button" onClick={handleDelete}>削除</button>
       </div>
-      {/* 編集ボタンと削除ボタン*/}
     </div>
-    
   );
 }
