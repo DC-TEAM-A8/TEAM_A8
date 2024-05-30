e9f17ce7007020cf841c8307cf3b8ed6edbe000f
Cabbage Lettuce <33714346+pycabbage@users.noreply.github.com>
2024-05-30 17:50:30 +0900

    ヘッダーのメニュー名変更、リンク先変更

--- web/src/app/layout.js
+++ web/src/app/layout.js
@@ -18,7 +18,7 @@ export default function RootLayout({ children }) {
           links = {{
           Report: "/dailyReport",
           result: "/test",
-          thoughts: "/thoughts"
+          thoughts: "/"
         }} />
         {children}
       </body>
